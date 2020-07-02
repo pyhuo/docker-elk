0c2f2f76968210eba69ee2756800a8dc10afc5f3
huoyinghui <yhhuo@yunjinginc.com>
2020-07-02 18:17:17 +0800

    add: docker-compose

--- run/beat.sh
+++ run/beat.sh
@@ -2,6 +2,7 @@
 docker run -d \
            --name=filebeat \
            --user=root \
+           --volume="$(pwd)/filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
            --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
            --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
            --network="docker-elk_elk" \
