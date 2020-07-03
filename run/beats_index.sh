da88eaa0f909eb0b7a8225289ac9d0bb61609cc5
mabo@yunjinginc.com <mabo@yunjinginc.com>
2020-07-03 09:55:12 +0800

    update: kibana dash

--- run/beats_index.sh
+++ run/beats_index.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-docker run  --network="docker-elk_elk" docker-elk_filebeat  setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["elasticsearch:9200"]'
+#docker run  --network="docker-elk_elk" docker-elk_filebeat  setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["elasticsearch:9200"]'
+docker run  --network="docker-elk_elk" docker-elk_filebeat  setup --dashboards -E 'output.elasticsearch.hosts=["elasticsearch:9200"]'
